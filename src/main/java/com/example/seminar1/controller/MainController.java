fc9c89c96dc47123064e5ccfc9608cf9c9f659d1
hskim <simplebest2k@naver.com>
2021-10-25 20:51:41 +0900

    MainController send1 api 추가 send2 api 추가

--- src/main/java/com/example/seminar1/controller/MainController.java
+++ src/main/java/com/example/seminar1/controller/MainController.java
@@ -1,7 +1,17 @@
 package com.example.seminar1.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+   @GetMapping("/send1")
+   public String send1() {
+      return "send1";
+   }
+
+   @GetMapping("/send2")
+   public String send2() {
+      return "send2";
+   }
 }
