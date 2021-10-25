528dd3a68f897183c5d06a2c0a30aea6ad78ab89
hskim <simplebest2k@naver.com>
2021-10-25 20:41:04 +0900

    EtcController echo api 추가

--- src/main/java/com/example/seminar1/controller/EtcController.java
+++ src/main/java/com/example/seminar1/controller/EtcController.java
@@ -1,7 +1,12 @@
 package com.example.seminar1.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EtcController {
+   @GetMapping("/echo")
+   public String echo() {
+      return "echo";
+   }
 }
